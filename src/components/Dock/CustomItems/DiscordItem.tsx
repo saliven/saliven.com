a2cfec4afc41f15563e8b0437d56994166c448e5
Saliven <s4liven@pm.me>
2023-01-01 18:49:13 +0100

    Add copy to clipboard function for discord dock item (fixes #5)

--- src/components/Dock/CustomItems/DiscordItem.tsx
+++ src/components/Dock/CustomItems/DiscordItem.tsx
@@ -1,14 +1,24 @@
 import { CopyIcon } from '@radix-ui/react-icons';
 import { FaDiscord } from 'react-icons/fa';
+import { useCopyToClipboard } from 'react-use';
 import { LanyardWebsocket } from 'react-use-lanyard/dist';
 import { Tooltip } from '../../Tooltip';
 import { DockItem } from '../DockItem';
 
 export function DiscordItem({ lanyard }: { lanyard: LanyardWebsocket }) {
+  const [, copyToClipboard] = useCopyToClipboard();
+
   return (
     <DockItem
       color="text-[#5865F2]"
       href=""
+      onClick={() =>
+        copyToClipboard(
+          lanyard.status?.discord_user.username +
+            '#' +
+            lanyard.status?.discord_user.discriminator
+        )
+      }
       tooltip={
         <Tooltip>
           <div className="flex items-center space-x-1">
