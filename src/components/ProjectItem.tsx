3c99835ec1c02ed5061b02dd9f0fdb3b0b93c5ab
Saliven <s4liven@pm.me>
2023-01-07 11:16:14 +0100

    Responsive design for `ProjectItem` component

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -12,9 +12,9 @@ export function ProjectItem({ name, description, year, link = '' }: Props) {
     <Link
       href={link}
       target="_blank"
-      className="group flex justify-between rounded-lg border border-gray-4 border-opacity-30 bg-opacity-30 p-4 transition-all duration-200 hover:border-opacity-100 hover:bg-gray-2/60"
+      className="group flex flex-col justify-between space-y-3 space-x-2 rounded-lg border border-gray-4 border-opacity-30 bg-opacity-30 p-4 transition-all duration-200 hover:border-opacity-100 hover:bg-gray-2/60 sm:flex-row sm:space-y-0"
     >
-      <div className="flex items-center space-x-1">
+      <div className="flex flex-col sm:flex-row sm:items-start sm:space-x-2">
         <h1 className="text-md font-medium transition-colors duration-200 group-hover:text-white">
           {name}
         </h1>
