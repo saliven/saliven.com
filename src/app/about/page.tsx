808d3454c5b354ac408bed37892c7a312966c63d
Saliven <s4liven@pm.me>
2024-03-05 14:13:27 +0100

    Add colors to technologies

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,18 +16,18 @@ import {
 import { TechnologyItem } from '../../components/TechnologyItem';
 
 const TECHNOLOGIES = [
-  { name: 'Docker', icon: <SiDocker size={20} /> },
-  { name: 'Terraform', icon: <SiTerraform size={20} /> },
-  { name: 'Elixir', icon: <SiElixir size={20} /> },
-  { name: 'Go', icon: <SiGo size={20} /> },
-  { name: 'Rust', icon: <SiRust size={20} /> },
-  { name: 'Node.js', icon: <SiNodedotjs size={20} /> },
-  { name: 'TypeScript', icon: <SiTypescript size={20} /> },
-  { name: 'React', icon: <SiReact size={20} /> },
-  { name: 'Next.js', icon: <SiNextdotjs size={20} /> },
-  { name: 'Git', icon: <SiGit size={20} /> },
-  { name: 'Tailwind', icon: <SiTailwindcss size={20} /> },
-  { name: 'PostgreSQL', icon: <SiPostgresql size={20} /> }
+  { name: 'Docker', icon: <SiDocker size={20} />, color: "#1D63ED" },
+  { name: 'Terraform', icon: <SiTerraform size={20} />, color: "#7B42BC" },
+  { name: 'Elixir', icon: <SiElixir size={20} />, color: "#A200C3" },
+  { name: 'Go', icon: <SiGo size={20} />, color: "#00ADD8" },
+  { name: 'Rust', icon: <SiRust size={20} />, color: "#FF9D3B" },
+  { name: 'Node.js', icon: <SiNodedotjs size={20} />, color: "#66CC33" },
+  { name: 'TypeScript', icon: <SiTypescript size={20} />, color: "#3178C6" },
+  { name: 'React', icon: <SiReact size={20} />, color: "#499CC6" },
+  { name: 'Next.js', icon: <SiNextdotjs size={20} />, color: "#FFFFFF" },
+  { name: 'Git', icon: <SiGit size={20} />, color: "#E15B39" },
+  { name: 'Tailwind', icon: <SiTailwindcss size={20} />, color: "#62BAF3" },
+  { name: 'PostgreSQL', icon: <SiPostgresql size={20} />, color: "#40668D" }
 ];
 
 export default function About() {
@@ -57,7 +57,7 @@ export default function About() {
 
         <div className="mt-8 grid grid-cols-1 gap-5 sm:grid-cols-3">
           {TECHNOLOGIES.map((t, i) => (
-            <TechnologyItem icon={t.icon} name={t.name} key={i} />
+            <TechnologyItem {...t} key={i} />
           ))}
         </div>
       </div>
